fb6804b49249f07d6c7e140a9c6b8fe1fcc54cbd
unika1 <unikaprj@gmail.com>
2026-01-03 15:44:18 +0545

    User repository created

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,11 +1,39 @@
-import User from "../models/user.model.js";
+import { UserModel, IUser } from "../models/user.model.js";
 
-export class UserRepository {
-  getUserByEmail(email: string) {
-    return User.findOne({ email });
+export interface IUserRepository {
+  getUserByEmail(email: string): Promise<IUser | null>;
+  createUser(userData: Partial<IUser>): Promise<IUser>;
+
+  getUserById(id: string): Promise<IUser | null>;
+  getAllUsers(): Promise<IUser[]>;
+  updateUser(id: string, updateData: Partial<IUser>): Promise<IUser | null>;
+  deleteUser(id: string): Promise<boolean>;
+}
+
+export class UserRepository implements IUserRepository {
+  async createUser(userData: Partial<IUser>): Promise<IUser> {
+    const user = new UserModel(userData);
+    return await user.save();
   }
 
-  createUser(data: any) {
-    return User.create(data);
+  async getUserByEmail(email: string): Promise<IUser | null> {
+    return await UserModel.findOne({ email });
+  }
+
+  async getUserById(id: string): Promise<IUser | null> {
+    return await UserModel.findById(id);
+  }
+
+  async getAllUsers(): Promise<IUser[]> {
+    return await UserModel.find();
+  }
+
+  async updateUser(id: string, updateData: Partial<IUser>): Promise<IUser | null> {
+    return await UserModel.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    const result = await UserModel.findByIdAndDelete(id);
+    return result ? true : false;
   }
 }
