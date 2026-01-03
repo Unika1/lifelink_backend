4f09ac68d0745b6d3c6f54328b7bfbee048cc723
unika1 <unikaprj@gmail.com>
2026-01-03 15:43:20 +0545

    User model created

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,24 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
+import { UserType } from "../types/user.types.js";
 
-const UserSchema = new mongoose.Schema(
+
+const UserSchema:Schema = new Schema <UserType> (
   {
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default:'user',
+    }
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", UserSchema);
+export interface IUser extends UserType, Document { // combine UserType and Document
+    _id: mongoose.Types.ObjectId; // mongo related attribute/ custom attributes
+    createdAt: Date;
+    updatedAt: Date;
+}
+export const UserModel = mongoose.model<IUser>('User', UserSchema);
