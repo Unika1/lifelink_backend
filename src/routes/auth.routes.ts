53c9f8b1edf1a7fa75105ed3b5aebcb0395e7dc0
unika1 <unikaprj@gmail.com>
2026-01-29 13:52:46 +0545

    auth route updated

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller.js";
+import { authorizedMiddleware } from "../middlewares/auth.middleware.js";
+import { uploads } from "../middlewares/upload.middleware.js";
 
 const router = Router();
 const controller = new AuthController();
+router.put(
+  "/update-profile",
+  authorizedMiddleware,
+  uploads.single("image"),
+  controller.updateProfile.bind(controller)
+);
+
 
 router.post("/register", controller.register.bind(controller));
 router.post("/login", controller.login.bind(controller));
