2abfaaa749b0177154bb18b6399c6574c4306019
unika1 <unikaprj@gmail.com>
2026-01-29 13:55:18 +0545

    index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,45 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import { connectDatabase } from './database/mongodb.js';
-import { PORT } from './config/index.js';
-import authRoutes from './routes/auth.routes.js';
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+
+import { connectDatabase } from "./database/mongodb.js";
+import { PORT } from "./config/index.js";
+import authRoutes from "./routes/auth.routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/auth', authRoutes);
-app.get('/', (req: Request, res: Response) => {
-    return res.status(200).json({ success: "true", message: "Welcome to the API" });
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    message: "Welcome to the LifeLink API",
+  });
 });
 
 async function startServer() {
-    await connectDatabase();
+  await connectDatabase();
 
-    app.listen(
-        PORT,
-        () => {
-            console.log(`Server: http://localhost:${PORT}`);
-        }
-    );
+  app.listen(PORT, () => {
+    console.log(`Server: http://localhost:${PORT}`);
+  });
 }
 
 startServer();
